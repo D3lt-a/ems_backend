6003443bab171c19ffefe7e1150da81a10df4950
Gavin Boris <kirengaboris07@gmail.com>
2026-04-02 05:57:05 +0200

    Finished the Department APIs

--- index.js
+++ index.js
@@ -13,12 +13,36 @@ const db = mysql.createPool({
 })
 
 try {
-    db.getConnection()
+    db.getConnection();
     console.log('[V] Database connection successful');
 } catch (error) {
     console.error('[X] Database connection failed:', error);
 }
 
+// =============== Department Routes ===============
+
+app.get('/departments', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM department');
+        console.log('[V] Departments retrieved successfully');
+        res.status(200).json({ message: 'Departments retrieved successfully', data: rows });
+    } catch (error) {
+        console.error('[X] Error retrieving departments:', error);
+        res.status(500).json({ message: 'Error retrieving departments' });
+    }
+})
+
+app.post('/departments/create', async (req, res) => {
+    const { code, name, salary } = req.body;
+    try {
+        await db.query('INSERT INTO department(DepartmentCode, DepartmentName, GrossSalary) VALUES (?, ?, ?)', [code, name, salary]);
+        console.log('[V] Department created successfully');
+        res.status(201).json({ message: 'Department created successfully' });
+    } catch (error) {
+        console.error('[X] Error creating department:', error);
+        res.status(500).json({ message: 'Error creating department' });
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running here: http://localhost:${port}`);
