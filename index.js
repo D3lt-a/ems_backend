cd9840b59a3d40de5326c802d71b257558a89ede
Gavin Boris <kirengaboris07@gmail.com>
2026-04-02 06:03:02 +0200

    Finished the Employees APIs

--- index.js
+++ index.js
@@ -44,6 +44,48 @@ app.post('/departments/create', async (req, res) => {
     }
 })
 
+// =============== Employees Routes ===============
+
+app.get('/employees', async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM employee');
+        console.log('[V] Employees retrieved successfully');
+        res.status(200).json({ message: 'Employees retrieved successfully', data: rows });
+    } catch (error) {
+        console.error('[X] Error retrieving employees:', error);
+        res.status(500).json({ message: 'Error retrieving employees' });
+    }
+})
+
+app.get('/employees/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [rows] = await db.query('SELECT * FROM employee WHERE employeeNumber = ?', [id]);
+        console.log('[V] Employee retrieved successfully');
+        res.status(200).json({ message: 'Employee retrieved successfully', data: rows });
+    } catch (error) {
+        console.error('[X] Error retrieving employee:', error);
+        res.status(500).json({ message: 'Error retrieving employee' });
+    }
+})
+
+app.post('/employees/create', async (req, res) => {
+    const { fname, lname, position, address, phone, gender, code } = req.body;
+    try {
+        await db.query('INSERT INTO employee(FirstName, LastName, Position, Address, Telephone, Gender, DepartmentCode) VALUES (?, ?, ?, ?, ?, ?, ?)', 
+            [fname, lname, position, address, phone, gender, code]);
+        console.log('[V] Employee created successfully');
+        res.status(201).json({ message: 'Employee created successfully' });
+    } catch (error) {
+        console.error('[X] Error creating employee:', error);
+        res.status(500).json({ message: 'Error creating employee' });
+    }
+})
+
+
+// =============== Department Routes ===============
+
+
 app.listen(port, () => {
     console.log(`Server is running here: http://localhost:${port}`);
 })
